0f4de77c0fce9e5513a54d84886d632e9dc4fb82
Tyler Hill <tyler@oxide.fi>
2021-04-20 08:18:38 -0500

    Change lifetime on convert_windows_string
    Signed-off-by: Tyler Hill <tyler@oxide.fi>

--- src/windows/utils.rs
+++ src/windows/utils.rs
@@ -2,6 +2,7 @@ use crate::windows::wrappers::{
     close_handle, create_tool_help32_snapshot, module32_first, module32_next, process32_first,
     process32_next, DWORD, DWORD_PTR,
 };
+use anyhow::Result;
 use bindings::Windows::Win32::SystemServices::{CHAR, INVALID_HANDLE_VALUE};
 use bindings::Windows::Win32::ToolHelp::{
     CREATE_TOOLHELP_SNAPSHOT_FLAGS, MODULEENTRY32, PROCESSENTRY32,
@@ -9,15 +10,11 @@ use bindings::Windows::Win32::ToolHelp::{
 use std::ffi::CStr;
 use std::{io::Error, mem::size_of};
 
-pub fn convert_windows_string<const N: usize>(string: [CHAR; N]) -> &'static str {
-    unsafe {
-        CStr::from_ptr(string.as_ptr() as *const i8)
-            .to_str()
-            .unwrap()
-    }
+pub fn convert_windows_string<'a, const N: usize>(string: [CHAR; N]) -> Result<&'a str> {
+    unsafe { Ok(CStr::from_ptr(string.as_ptr() as *const i8).to_str()?) }
 }
 
-pub fn get_process_id(process_name: &str) -> Result<DWORD, Error> {
+pub fn get_process_id(process_name: &str) -> Result<DWORD> {
     let mut process_id: DWORD = 0;
 
     let snapshot = create_tool_help32_snapshot(
@@ -26,7 +23,7 @@ pub fn get_process_id(process_name: &str) -> Result<DWORD, Error> {
     );
 
     if snapshot == INVALID_HANDLE_VALUE {
-        return Err(Error::last_os_error());
+        return Err(Error::last_os_error().into());
     }
 
     let mut entry = PROCESSENTRY32 {
@@ -36,7 +33,7 @@ pub fn get_process_id(process_name: &str) -> Result<DWORD, Error> {
 
     if process32_first(snapshot, &mut entry) {
         process_id = loop {
-            let current_name = convert_windows_string(entry.szExeFile);
+            let current_name = convert_windows_string(entry.szExeFile)?;
 
             if current_name == process_name {
                 break entry.th32ProcessID;
@@ -51,13 +48,13 @@ pub fn get_process_id(process_name: &str) -> Result<DWORD, Error> {
     }
 
     if process_id == 0 {
-        return Err(Error::last_os_error());
+        return Err(Error::last_os_error().into());
     }
 
     Ok(process_id)
 }
 
-pub fn get_module_base(process_id: DWORD, module_name: &str) -> Result<DWORD_PTR, Error> {
+pub fn get_module_base(process_id: DWORD, module_name: &str) -> Result<DWORD_PTR> {
     let mut module_base_address: DWORD_PTR = 0x0;
 
     let snapshot = create_tool_help32_snapshot(
@@ -72,7 +69,7 @@ pub fn get_module_base(process_id: DWORD, module_name: &str) -> Result<DWORD_PTR
 
     if module32_first(snapshot, &mut entry) {
         module_base_address = loop {
-            let current_name = convert_windows_string(entry.szModule);
+            let current_name = convert_windows_string(entry.szModule)?;
 
             if current_name == module_name {
                 break entry.modBaseAddr as DWORD_PTR;
@@ -87,7 +84,7 @@ pub fn get_module_base(process_id: DWORD, module_name: &str) -> Result<DWORD_PTR
     }
 
     if module_base_address == 0 {
-        return Err(Error::last_os_error());
+        return Err(Error::last_os_error().into());
     }
 
     Ok(module_base_address)
