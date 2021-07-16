bd32113ee5e0c4c4b86c42451332f41844155123
Steele Scott <60533996+steele123@users.noreply.github.com>
2021-07-16 02:05:55 -0400

    Windows bindings for directx stuff

--- mem/bindings/build.rs
+++ mem/bindings/build.rs
@@ -7,12 +7,17 @@ fn main() {
         Windows::Win32::System::LibraryLoader::{GetModuleHandleW, FreeLibraryAndExitThread, DisableThreadLibraryCalls, GetModuleHandleA, GetProcAddress},
 
         Windows::Win32::System::Console::{AllocConsole, FreeConsole},
+
         // Direct X
         // D3D9
         Windows::Win32::Graphics::Direct3D9::{D3D_SDK_VERSION, Direct3DCreate9, D3DPRESENT_PARAMETERS, IDirect3D9},
         // D3D10
         Windows::Win32::Graphics::Dxgi::{CreateDXGIFactory, IDXGIFactory, DXGI_RATIONAL, DXGI_MODE_DESC, DXGI_FORMAT, DXGI_SAMPLE_DESC, DXGI_SWAP_CHAIN_DESC, DXGI_SWAP_CHAIN_FLAG, DXGI_SWAP_EFFECT},
         Windows::Win32::Graphics::Direct3D10::{D3D10CreateDeviceAndSwapChain, D3D10_SDK_VERSION},
+        // D3D11
+        Windows::Win32::Graphics::Direct3D11::{D3D11CreateDeviceAndSwapChain, D3D_FEATURE_LEVEL},
+        // D3D12
+        Windows::Win32::Graphics::Direct3D12::{D3D12CreateDevice, ID3D12Device, D3D12_COMMAND_QUEUE_DESC, ID3D12CommandQueue, ID3D12CommandAllocator, ID3D12GraphicsCommandList},
 
         Windows::Win32::UI::WindowsAndMessaging::{WNDCLASSEXW, CreateWindowExW, DestroyWindow, UnregisterClassW},
 
